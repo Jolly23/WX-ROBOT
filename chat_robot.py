63197164ab62e9fe7d262d4a41cde6c3bc77032a
Jolly23 <pilot_lei@foxmail.com>
2017-04-15 16:27:52 +0800

    new

---
+++ chat_robot.py
@@ -0,0 +1,26 @@
+# coding=utf8
+import json
+import requests
+from hashlib import md5
+
+
+def chat_api(text, user_id=None):
+    payload = {
+        'key': 'http://www.tuling123.com/openapi/api',
+        'info': text,
+    }
+    if user_id:
+        payload['userid'] = md5(str(user_id)).hexdigest()
+    try:
+        r_data = requests.get(
+            url='http://www.tuling123.com/openapi/api',
+            params=payload,
+            timeout=1.5
+        )
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+        return u'机器人可能生病了，睡得很香，暂时不能陪你聊天啦'
+    data = json.loads(r_data.text)
+    text = data.get('text')
+    if data.get('code') == 200000:
+        text += u', <a href="{}">点击查看</a>'.format(data.get('url'))
+    return text
